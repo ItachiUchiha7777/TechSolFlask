6a2f0239501d204b06b9171d97cf955fd4cc9f0b
Rohit Gusain <98213628+ItachiUchiha7777@users.noreply.github.com>
2025-08-27 20:31:38 +0530

    added privacy.html

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ def service_category(category):
 @app.route('/partner')
 def partner():
     return render_template('partner.html', services_data=SERVICES_DATA)
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html', services_data=SERVICES_DATA)
 
 @app.route('/service/<category>/<service_name>')
 def service_detail(category, service_name):
